270aedb5ecea059a3c5c13dd9793e9270287dcd4
Charles Bueche <bueche@netnea.com>
2016-11-30 13:45:58 +0100

    enhance templates and documentation

--- aj/app/etc/templates/credentials_template.py
+++ aj/app/etc/templates/credentials_template.py
@@ -11,24 +11,43 @@ Rename this file to credentials.py before usage.
 
 # -----------------------------------------------------------------------------------
 class Credentials():
+	'''
+	A wrapper to get device credentials
 
-    '''
-    A wrapper to get device credentials
+	This is a callback that Agent-Jones uses to get credentials for a device.
+	You will need to adapt this to return your communities and login, or with a more
+	elaborate code, eg getting them from a CDMB.
 
-    This is a callback that Agent-Jones uses to get credentials for a device.
-    You will need to adapt this to return your communities and login, or with a more
-    elaborate code, eg getting them from a CDMB.
+	The function gets the devicename as parameter, and must return a dict containing
+	the SNMP communities, user and password. Alternatively, the SNMPv3 credentials.
+	'''
 
-    The function gets the devicename as parameter, and must return a dict containing
-    the SNMP communities, user and password.
-    '''
+	def get_credentials(self, devicename):
 
-    def get_credentials(self, devicename):
-  
-        return {
-            'ro_community': 'public',
-            'rw_community': 'private',
-            'username': 'switch_user',
-            'password': 'switch_password',
-            'password_enable': 'switch_password_enable'
-        }
+		if devicename == 'switch-snmp2':
+			return {
+				'ro_community': 'public',
+				'rw_community': 'private',
+				'username': 'switch_user',
+				'password': 'switch_password',
+				'snmp_version': 2,
+				'secname': None,
+				'authprotocol': None,
+				'authpassword': None,
+				'privprotocol': None,
+				'privpassword': None
+			}
+
+		if devicename in ['router-snmp3', '192.168.22.60']:
+			return {
+				'ro_community': None,
+				'rw_community': '',
+				'username': 'FIXME',
+				'password': '',
+				'snmp_version': 3,
+				'secname': 'MYUSER',
+				'authprotocol': 'MD5',
+				'authpassword': 'MYPASS456',
+				'privprotocol': 'AES',
+				'privpassword': 'MYKEY678'
+			}
